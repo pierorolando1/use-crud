26b2661ab205d79aff910f0f01ef17b8659ea385
Piero Rolando <rolandopiero63@gmail.com>
2022-05-23 11:07:20 -0400

    create folder structure

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+import Provider from './Provider';
+
+// Delete me
+export const Thing = () => {
+	return <div>the snozzberries taste like snozzberries</div>;
+};
+
+export Provider
