8b57adf1a3602821c1830b0523d88125b0fa370f
Piero Rolando <rolandopiero63@gmail.com>
2022-05-23 22:41:32 -0400

    modules

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 
 export { Provider } from './Provider';
+export { default } from "./useCrud"
 
 // Delete me
 export const Thing = () => {
 	return <div>the snozzberries taste like snozzberries</div>;
-};
-
+}
