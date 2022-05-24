bf0c4f7a4c86ecc73bc735bfbcfbef2d5991c380
Piero Rolando <rolandopiero63@gmail.com>
2022-05-23 22:41:39 -0400

    Provider component

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,7 +1,22 @@
+import { MantineProvider } from "@mantine/core"
+import { ModalsProvider } from "@mantine/modals"
 import React, { FC } from "react"
+import { Context } from "./context"
 
-export const Provider: FC<{}> = () => {
-	return (
-		<></>
-	)
+type ProviderProps = {
+    children: React.ReactNode,
+}
+
+export const Provider: FC<ProviderProps> = ({
+    children,
+}) => {
+    return (
+        <Context.Provider value={null}>
+            <MantineProvider>
+                <ModalsProvider>
+                    {children}
+                </ModalsProvider>
+            </MantineProvider>
+        </Context.Provider>
+    )
 }
