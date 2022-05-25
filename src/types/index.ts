5d3affb0ef11dd4d0c4adeed2e6f97a3b6206d9d
Piero Rolando <rolandopiero63@gmail.com>
2022-05-24 22:26:16 -0400

    add new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,18 @@
-import { FirebaseOptions } from "firebase/app"
-
 export type InputType = "text" | "video" | "image"
 
 export type FormField<T> = { type: InputType, field: keyof T, label: string }
 
 export type useCrudProps<U> = {
-    firebaseConfig: FirebaseOptions,
-
     /// Collection in FIRESTORE that you want to interact
-    collection: string,
+    c: string,
     onCreate?: (arg0: U) => void,
     onUpdate?: (arg0: U) => void,
     onDelete?: () => void,
     formFields: FormField<U>[],
     label: string
+}
+
+export type useCreateOrUpdateProps<U> = {
+    collection: string,
+    formFields: FormField<U>[],
 }
