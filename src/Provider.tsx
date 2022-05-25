d21a61bbf32dddafe608518d351bf6f0e53149dc
Piero Rolando <rolandopiero63@gmail.com>
2022-05-24 22:25:49 -0400

    create a real context

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,17 +1,20 @@
 import { MantineProvider } from "@mantine/core"
 import { ModalsProvider } from "@mantine/modals"
+import type { FirebaseOptions } from "firebase/app"
 import React, { FC } from "react"
 import { Context } from "./context"
 
 type ProviderProps = {
     children: React.ReactNode,
+    firebaseConfig: FirebaseOptions
 }
 
 export const Provider: FC<ProviderProps> = ({
     children,
+    firebaseConfig
 }) => {
     return (
-        <Context.Provider value={null}>
+        <Context.Provider value={{ firebaseConfig }}>
             <MantineProvider>
                 <ModalsProvider>
                     {children}
--- src/context.tsx
+++ src/context.tsx
@@ -1,3 +1,6 @@
+import { FirebaseOptions } from "firebase/app";
 import { createContext } from "react";
 
-export const Context = createContext(null)
+export const Context = createContext<{ firebaseConfig: FirebaseOptions }>({
+    firebaseConfig: {}
+})
