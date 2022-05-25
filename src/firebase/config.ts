08f56a9aa9f956279428c9f3bc5a3c59280d0fb9
Piero Rolando <rolandopiero63@gmail.com>
2022-05-24 22:26:06 -0400

    add storage

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,15 @@
-import { FirebaseOptions, initializeApp } from 'firebase/app'
+import { initializeApp } from 'firebase/app'
 import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
+import { useContext } from 'react'
+import { Context } from '../context'
 
-export const useFirebase = (config: FirebaseOptions) => {
-    const app = initializeApp(config)
+export const useFirebase = () => {
+    const { firebaseConfig } = useContext(Context)
+    const app = initializeApp(firebaseConfig)
 
     return {
-        db: getFirestore(app)
+        db: getFirestore(app),
+        storage: getStorage(app)
     }
 }
